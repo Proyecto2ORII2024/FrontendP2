2387546237049ccad56a3d84485e39e6aebec0e8
Jaycom17 <juanorejuela499@gmail.com>
2024-08-24 10:58:41 -0500

    se agrego la configuracion de tailwind y el enrutado con react-router-dom

---
+++ src/pages/mainAdminPage/MainAdmin.jsx
@@ -0,0 +1,9 @@
+function MainAdmin() {
+  return (
+    <div>
+      <h1>Main Admin Page</h1>
+    </div>
+  );
+}
+
+export default MainAdmin;
---
+++ src/routes/Router.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "../pages/loginPage/Login";
+import MainAdmin from "../pages/mainAdminPage/MainAdmin";
+import MainUser from "../pages/mainUserPage/MainUser";
+
+function Router() {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/admin" element={<MainAdmin />} />
+            <Route path="/user" element={<MainUser />} />
+        </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default Router;
