ab8319578930ab54bbc8368ce90e40accc2d091a
Jaycom17 <juanorejuela499@gmail.com>
2024-09-10 12:40:09 -0500

    se empezo con la vista de convenio admin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkbluebutton: "#000066",
+      }
+    },
   },
   plugins: [],
 }
