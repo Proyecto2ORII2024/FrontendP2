f5fb8e1d53f94b0af7fd5a21bf7f7f0f8fabdefc
Jaycom17 <juanorejuela499@gmail.com>
2024-09-19 21:27:38 -0500

    button component and convenio admin page

--- src/pages/convenioAdminPage/ConvenioAdminPage.jsx
+++ src/pages/convenioAdminPage/ConvenioAdminPage.jsx
@@ -1,7 +1,10 @@
 import deleteIcon from "../../assets/icons/deleteIcon.svg";
 import editIcon from "../../assets/icons/editIcon.svg";
 
-import { styles } from "./styles.js";
+import Accordeon from "../../components/acordeonBox/Acordeon.jsx";
+import MainButton from "../../components/buttons/MainButton.jsx";
+
+import styles from "./styles.js";
 
 function ConvenioAdminPage() {
   const convenios = [
@@ -9,6 +12,7 @@ function ConvenioAdminPage() {
       pais: "Alemania",
       institucion: "SERVICIO ALEMÁN DE INTERCAMBIO ACADÉMICO (DAAD)",
       codigo: "25-32.7 040 2023",
+      fechaInicio: "2023-01-01",
       descripcion:
         "Otorgar una subvención no reembolsable para la financiación de proyectos en el programa de financiación viajes de estudio para grupos de estudiantes extranjeros en Alemania, con fondos del Ministerio Federal de Relaciones Exteriores.",
     },
@@ -16,41 +20,34 @@ function ConvenioAdminPage() {
       pais: "Alemania",
       institucion: "SERVICIO ALEMÁN DE INTERCAMBIO ACADÉMICO (DAAD)",
       codigo: "25-32.7 040 2023",
+      fechaInicio: "2023-01-01",
       descripcion:
         "Otorgar una subvención no reembolsable para la financiación de proyectos en el programa de financiación viajes de estudio para grupos de estudiantes extranjeros en Alemania, con fondos del Ministerio Federal de Relaciones Exteriores.",
     },
-    
     {
-        pais: "Alemania",
-        institucion: "SERVICIO ALEMÁN DE INTERCAMBIO ACADÉMICO (DAAD)",
-        codigo: "25-32.7 040 2023",
-        descripcion:
-          "Otorgar una subvención no reembolsable para la financiación de proyectos en el programa de financiación viajes de estudio para grupos de estudiantes extranjeros en Alemania, con fondos del Ministerio Federal de Relaciones Exteriores.",
-      },
-      
+      pais: "Alemania",
+      institucion: "SERVICIO ALEMÁN DE INTERCAMBIO ACADÉMICO (DAAD)",
+      codigo: "25-32.7 040 2023",
+      fechaInicio: "2023-01-01",
+      descripcion:
+        "Otorgar una subvención no reembolsable para la financiación de proyectos en el programa de financiación viajes de estudio para grupos de estudiantes extranjeros en Alemania, con fondos del Ministerio Federal de Relaciones Exteriores.",
+    },
     {
-        pais: "Alemania",
-        institucion: "SERVICIO ALEMÁN DE INTERCAMBIO ACADÉMICO (DAAD)",
-        codigo: "25-32.7 040 2023",
-        descripcion:
-          "Otorgar una subvención no reembolsable para la financiación de proyectos en el programa de financiación viajes de estudio para grupos de estudiantes extranjeros en Alemania, con fondos del Ministerio Federal de Relaciones Exteriores.",
-      },
-      
+      pais: "Alemania",
+      institucion: "SERVICIO ALEMÁN DE INTERCAMBIO ACADÉMICO (DAAD)",
+      codigo: "25-32.7 040 2023",
+      fechaInicio: "2023-01-01",
+      descripcion:
+        "Otorgar una subvención no reembolsable para la financiación de proyectos en el programa de financiación viajes de estudio para grupos de estudiantes extranjeros en Alemania, con fondos del Ministerio Federal de Relaciones Exteriores.",
+    },
     {
-        pais: "Alemania",
-        institucion: "SERVICIO ALEMÁN DE INTERCAMBIO ACADÉMICO (DAAD)",
-        codigo: "25-32.7 040 2023",
-        descripcion:
-          "Otorgar una subvención no reembolsable para la financiación de proyectos en el programa de financiación viajes de estudio para grupos de estudiantes extranjeros en Alemania, con fondos del Ministerio Federal de Relaciones Exteriores.",
-      },
-      
-    {
-        pais: "Alemania",
-        institucion: "SERVICIO ALEMÁN DE INTERCAMBIO ACADÉMICO (DAAD)",
-        codigo: "25-32.7 040 2023",
-        descripcion:
-          "Otorgar una subvención no reembolsable para la financiación de proyectos en el programa de financiación viajes de estudio para grupos de estudiantes extranjeros en Alemania, con fondos del Ministerio Federal de Relaciones Exteriores.",
-      },
+      pais: "Alemania",
+      institucion: "SERVICIO ALEMÁN DE INTERCAMBIO ACADÉMICO (DAAD)",
+      codigo: "25-32.7 040 2023",
+      fechaInicio: "2023-01-01",
+      descripcion:
+        "Otorgar una subvención no reembolsable para la financiación de proyectos en el programa de financiación viajes de estudio para grupos de estudiantes extranjeros en Alemania, con fondos del Ministerio Federal de Relaciones Exteriores.",
+    },  
   ];
 
   return (
@@ -62,13 +59,10 @@ function ConvenioAdminPage() {
           funcionarios de las facultades.
         </h2>
         <section className="w-full mb-5">
-          <button
-            className={`${styles.blueButton} xl:ml-48 lg:ml-32 md:ml-20 ml-10`}
-          >
-            Crear Convenio
-          </button>
+          <MainButton text="Crear Convenio" bgColor="primary" hoverBg="primary-light" textColor="white" className="xl:ml-48 lg:ml-32 md:ml-20 ml-10"/>
         </section>
-        <section className="w-full flex justify-center px-5 lg:px-20 mb-5 max-h-screen overflow-auto border md:border-none">
+        <Accordeon title="Nacional">
+        <section className="w-full flex justify-center mb-5 max-h-screen overflow-auto border md:border-none">
           <table className="w-full text-left table-auto border-collapse md:table">
             <thead className="hidden md:table-header-group">
               <tr className="bg-[#928F9A]">
@@ -76,6 +70,7 @@ function ConvenioAdminPage() {
                 <th className={`${styles.thIn} w-[300px]`}>Institución</th>
                 <th className={`${styles.thIn} w-[200px]`}>Codigo</th>
                 <th className={`${styles.thIn} w-[350px]`}>Descripción</th>
+                <th className={`${styles.thIn} w-[200px]`}>Fecha de inicio</th>
                 <th className={`${styles.thIn} w-[250px]`}>Acciones</th>
                 <th className={`${styles.thIn} w-[250px]`}>Seleccionar</th>
               </tr>
@@ -103,6 +98,10 @@ function ConvenioAdminPage() {
                   <td className={styles.tdIn}>
                     <span className="md:hidden font-bold">Descripción: </span>
                     {convenio.descripcion}
+                  </td>
+                  <td className={styles.tdIn}>
+                    <span className="md:hidden font-bold">Fecha de inicio: </span>
+                    {convenio.fechaInicio}
                   </td>
                   <td className={styles.tdIn}>
                     <div className="flex md:justify-center items-center">
@@ -136,12 +135,84 @@ function ConvenioAdminPage() {
             </tbody>
           </table>
         </section>
+          </Accordeon>
+        <Accordeon title="Internacional">
+        <section className="w-full flex justify-center mb-5 max-h-screen overflow-auto border md:border-none">
+          <table className="w-full text-left table-auto border-collapse md:table">
+            <thead className="hidden md:table-header-group">
+              <tr className="bg-[#928F9A]">
+                <th className={`${styles.thIn} w-[150px]`}>Pais</th>
+                <th className={`${styles.thIn} w-[300px]`}>Institución</th>
+                <th className={`${styles.thIn} w-[200px]`}>Codigo</th>
+                <th className={`${styles.thIn} w-[350px]`}>Descripción</th>
+                <th className={`${styles.thIn} w-[200px]`}>Fecha de inicio</th>
+                <th className={`${styles.thIn} w-[250px]`}>Acciones</th>
+                <th className={`${styles.thIn} w-[250px]`}>Seleccionar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {convenios.map((convenio, index) => (
+                <tr
+                  className={`${
+                    index % 2 != 0 ? "md:bg-[#E4E1EC]" : "md:bg-[#FBF8FF]"
+                  } flex flex-col md:table-row border-b`}
+                  key={convenio.codigo}
+                >
+                  <td className={styles.tdIn}>
+                    <span className="md:hidden font-bold">Pais: </span>
+                    {convenio.pais}
+                  </td>
+                  <td className={styles.tdIn}>
+                    <span className="md:hidden font-bold">Instiución: </span>
+                    {convenio.institucion}
+                  </td>
+                  <td className={styles.tdIn}>
+                    <span className="md:hidden font-bold">Cosido: </span>
+                    {convenio.codigo}
+                  </td>
+                  <td className={styles.tdIn}>
+                    <span className="md:hidden font-bold">Descripción: </span>
+                    {convenio.descripcion}
+                  </td>
+                  <td className={styles.tdIn}>
+                    <span className="md:hidden font-bold">Fecha de inicio: </span>
+                    {convenio.fechaInicio}
+                  </td>
+                  <td className={styles.tdIn}>
+                    <div className="flex md:justify-center items-center">
+                      <span className="md:hidden font-bold">Acciones: </span>
+                      <div className="flex md:gap-0 justify-around w-[80%]">
+                        <button>
+                          <img
+                            className={styles.buttonAction}
+                            src={editIcon}
+                            alt="editIcom"
+                          />
+                        </button>
+                        <button>
+                          <img
+                            className={styles.buttonAction}
+                            src={deleteIcon}
+                            alt="deleteIcon"
+                          />
+                        </button>
+                      </div>
+                    </div>
+                  </td>
+                  <td className={styles.tdOut}>
+                    <div className="flex md:justify-center gap-2">
+                      <span className="md:hidden font-bold">Seleccionar: </span>
+                      <input className="h-[25px] w-[25px]" type="checkbox" />
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
+          </Accordeon>
         <section className="w-full mb-5 flex justify-end">
-          <button
-            className={`${styles.blueButton} xl:mr-48 lg:mr-32 md:mr-20 mr-10`}
-          >
-            Guardar Selecciones
-          </button>
+          <MainButton text="Guardar Selecciones" bgColor="primary" hoverBg="primary-light" textColor="white" className="mt-5 xl:mr-48 lg:mr-32 md:mr-20 mr-10"/>
         </section>
       </main>
     </>
--- src/pages/convenioAdminPage/styles.js
+++ src/pages/convenioAdminPage/styles.js
@@ -1,9 +1,10 @@
-export const styles = {
+const styles = {
   blueButton:
-    "px-5 py-1 rounded-[20px] font-semibold text-white text-lg hover:bg-[#5056AC] duration-200",
+    "px-5 py-1 rounded-[20px] bg-primary font-semibold text-white text-lg hover:bg-primary-light duration-200",
   thIn: "border-r-2 py-3 text-xl text-center",
   thOut: "py-3 text-xl text-center",
   tdIn: "text-start md:text-center border-white p-2 border-r-2",
   tdOut: "text-start md:text-center border-white p-2",
   buttonAction: "h-[50px] w-[50px] hover:scale-105 duration-200",
 };
+export default styles;
