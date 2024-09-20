756e58a20bac1fa34d15ebf05ad9900cf8ee40f8
juanes2303.je@gmail.com <juanes2303.je@gmail.com>
2024-09-20 13:08:07 -0500

    nada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Router from "./routes/Router";
-
+import MainButton from "./components/buttons/MainButton.jsx";
+import InfoBubble from "./components/infoBubble/InfoBubble.jsx";
+/*<MainButton onclick={()=>{}} bgColor={"#000000"} hoverBg={} textColor text className />
+  <InfoBubble info={info}></InfoBubble>
+*/
+const info =
+{
+  title: "Text",
+  shortInfo: "Text",
+  longInfo: "informacion larga",
+};
 function App() {
 
   return (
-    <>
-      <Router/>
-    </>
+    <Router/>
   )
 }
 
