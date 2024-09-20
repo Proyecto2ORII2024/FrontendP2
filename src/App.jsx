f6fdfd8c2b9df24c6a5c4f0eaa4025c34f362e6b
Jaycom17 <juanorejuela499@gmail.com>
2024-09-19 21:30:39 -0500

    some mistakes were fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 import Router from "./routes/Router";
 
 function App() {
   return (
     <>
       <Router/>
-=======
-import Router from "./routes/Router"
-
-function App() {
-
-  return (
-    <>
-    <Router />
->>>>>>> ab8319578930ab54bbc8368ce90e40accc2d091a
     </>
   )
 }
