ce556cbcf2083c5820b7c5e886534220b328ffff
CarlMacGar <149346841+CarlMacGar@users.noreply.github.com>
2024-09-21 10:32:35 -0500

    Adaptación de componentes

--- src/pages/formPage/FormPage.jsx
+++ src/pages/formPage/FormPage.jsx
@@ -1,22 +1,11 @@
 import InfoBubble from "../../components/infoBubble/InfoBubble";
+import { Info } from "./Information.js";
 
 function FormPage(){
-    const info = {
-        convenio: {
-            title: "¿Existe convenio con la universidad de origen o destino?",
-            shortInfo: "Puede verificar si existe convenio, en el siguiente enlace:  http://www.unicauca.edu.co/orii/es/convenios",
-        },
-        uniOgDt:{
-            title: "Sentido",
-            longInfo: "Puede verificar si existe convenio, en el siguiente enlace:  http://www.unicauca.edu.co/orii/es/convenios",
-        }
-    }
-
     //Sigue en desarrollo, por ahora es un ejemplo de visualización de la burbuja
     return(
         <div className="flex flex-col gap-32">
-            <InfoBubble info={info.convenio}/>
-            <InfoBubble info={info.uniOgDt}/>
+            <InfoBubble info={Info.sentido}/>
         </div>
     );
 }
