1aadb0a6dc0c40a82cf94adbd15c789d5548297a
Juan Camilo Zarta Campo <juancamilozartacampo@gmail.com>
2024-09-25 21:10:03 -0500

    UpDate MainUserPage - Routes

--- src/pages/mainUserPage/MainUser.jsx
+++ src/pages/mainUserPage/MainUser.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import styles from './styles';  
+import styles from './styles';
+import { useNavigate } from 'react-router-dom';
 
 function MainUser() {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.mainContainer}>
       <h2 className="w-full p-5 text-lg text-center">
@@ -9,11 +12,16 @@ function MainUser() {
         las cuales también están siempre accesibles en el menú de la izquierda.
       </h2>
       <div className={styles.optionsContainer}>
-        <div className={styles.optionCard}>
+        <div
+          className={styles.optionCard}
+          onClick={() => navigate('/admin/convenio/')} 
+        >
           <img src="/convenios.svg" alt="Icono Convenios" className={styles.icon} />
           <p style={{ color: '#000066', fontWeight: 'bold' }}>Convenios</p>
         </div>
-        <div className={styles.optionCard}>
+        <div className={styles.optionCard}
+          onClick={() => navigate('/form')} 
+        >
           <img src="/formularios.svg" alt="Icono Formularios" className={styles.icon} />
           <p style={{ color: '#000066', fontWeight: 'bold' }}>Formularios</p>
         </div>
