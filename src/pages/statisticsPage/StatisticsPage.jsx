71c032578fee19e12808d404c897981e0d254621
freider45 <freiderescobar@unicauca.edu.co>
2024-09-28 16:29:36 -0500

    Add statistics graphs

--- src/pages/statisticsPage/StatisticsPage.jsx
+++ src/pages/statisticsPage/StatisticsPage.jsx
@@ -1,13 +1,62 @@
-
-import PieChart from "../../components/statistics/PieChart";
+import PieChartAgreements from "../../components/statistics/pieGraphs/PieChartAgreements";
+import BarChartMobility from "../../components/statistics/barGraphs/BarChartMobility";
+import LineChartMobilityTrend from "../../components/statistics/lineGraphs/LineChartMobilityTrend";
+import BarChartMobilityByDepartment from "../../components/statistics/barGraphs/BarChartMobilityByDepartment";
+import BarChartMobilityByCountries from "../../components/statistics/barGraphs/BarChartMobilityByCountries";
+import BarChartMobilitiesPerYear from "../../components/statistics/barGraphs/BarChartMobilitiesPerYear";
 
 const StatisticsPage = () => {
   return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-6">Estadísticas de movilidad</h1>
-      <div className="flex flex-col space-y-6">
-        <div className="bg-gradient-to-r to-purple-500 p-4 rounded-lg shadow-lg">
-          <PieChart />
+    <div>
+      <div className="mx-10 my-5 text-2xl font-semibold">
+        <h1>Estadísticas de movilidad</h1>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mx-10 my-10">
+        <div className="border border-gray-100 bg-white rounded-2xl h-96 shadow hover:shadow-lg">
+          <div className="flex justify-center mt-5">
+            <h2>Distribución de tipos de convenios</h2>
+          </div>
+          <PieChartAgreements />
+        </div>
+        <div className="border border-gray-100 bg-white rounded-2xl h-96 shadow hover:shadow-lg">
+          <div className="flex justify-center mt-5">
+            <h2>Movilidad por facultad</h2>
+          </div>
+          <div className="mt-10">
+            <BarChartMobility />
+          </div>
+        </div>
+        <div className="border border-gray-100 bg-white rounded-2xl h-96 shadow hover:shadow-lg">
+          <div className="flex justify-center mt-5">
+            <h2>Tendencia de movilidad anual</h2>
+          </div>
+          <div className="mt-10">
+            <LineChartMobilityTrend />
+          </div>
+        </div>
+        <div className="border border-gray-100 bg-white rounded-2xl h-96 shadow hover:shadow-lg">
+          <div className="flex justify-center mt-5">
+            <h2>Movilidades por año</h2>
+          </div>
+          <div className="mt-10">
+            <BarChartMobilitiesPerYear />
+          </div>
+        </div>
+        <div className="border border-gray-100 bg-white rounded-2xl h-96 shadow hover:shadow-lg">
+          <div className="flex justify-center mt-5">
+            <h2>Movilidades por países</h2>
+          </div>
+          <div className="mt-10">
+            <BarChartMobilityByCountries />
+          </div>
+        </div>
+        <div className="border border-gray-100 bg-white rounded-2xl h-96 shadow hover:shadow-lg">
+          <div className="flex justify-center mt-5">
+            <h2>Movilidades por departamentos</h2>
+          </div>
+          <div className="mt-10">
+            <BarChartMobilityByDepartment />
+          </div>
         </div>
       </div>
     </div>
