007d4c3ac2999558a0a0a53b7ced0aa5602bd888
FranSotelo2303 <149346738+FranSotelo2303@users.noreply.github.com>
2024-10-21 18:42:45 -0500

    Update Login.jsx

--- src/pages/loginPage/Login.jsx
+++ src/pages/loginPage/Login.jsx
@@ -1,10 +1,25 @@
 import LoginImage from "../../assets/Images/Login.webp";
-import GoogleIcon from "../../assets/icons/googleIcon.svg";
-import loginIcon from "../../assets/icons/loginIcon.svg";
 import ORIIIcon from "../../assets/Images/ORII.webp";
+import MainButton from "../../components/buttons/MainButton.jsx";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 //
 function Login() {
+
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const handleLogin = (data) => {
+    navigate("/admin")
+    console.log(data)
+  };
 
   return (
     <div
@@ -14,15 +29,46 @@ function Login() {
       <div className="flex flex-col items-center bg-primary/50 rounded-3xl gap-3 w-[90%] md:w-[50%] lg:w-[25%] md:py-10 py-5 justify-center">
         <img src={ORIIIcon} alt="ORIIIcon" className="w-[300px]" />
         <h1 className="bg-transparent text-4xl font-bold text-center">Bienvenido</h1>
-        <form className="flex flex-col items-center gap-y-2">
-          <label htmlFor="" className="text-xl">Ingresa con tu cuenta de Google</label>
-          <div className="flex items-center gap-2">
-            <img src={loginIcon} alt="loginIcon" className="w-[30px]" />
-            <button className="bg-white rounded-full p-3 text-black flex items-center gap-2">
-              <img src={GoogleIcon} alt="GoggleIcon" className="w-[30px]" />
-              Conectarse con Google
-            </button>
+        <form className="flex flex-col items-center gap-y-2" onSubmit={handleSubmit(handleLogin)}>
+          <label htmlFor="" className="text-xl">Ingresa con tu correo y contraseña</label>
+          <div className="flex flex-col gap-y-2 bg-primary/75 rounded-3xl p-3 py-4 items-start w-[90%]">
+            <div className="flex flex-col w-full">
+              <label htmlFor="" className="text-xl">Correo</label>
+              <input
+                htmlFor=""
+                className="bg-primary-light w-[100%]"
+                type="email"
+                {...register("email", {
+                  required: true, pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: "Correo no valido",
+                  },
+                })}
+              />
+              {errors.email && <span className="text-sm text-red-400 border-b-2 border-b-red-400 ml-2">
+                {errors.email.message || "Este campo es requerido"}
+              </span>}
+            </div>
+            <div className="flex flex-col w-full">
+              <label htmlFor="" className="text-xl">Contraseña</label>
+              <input
+                htmlFor=""
+                className="bg-primary-light w-[100%]"
+                type="password"
+                {...register("password", { required: true })} />
+              {errors.password && <span className="text-sm text-red-400 border-b-2 border-b-red-400 ml-2">
+                {errors.password.message || "Este campo es requerido"}
+              </span>}
+            </div>
           </div>
+          <MainButton
+            type="submit"
+            text="Iniciar Sesion"
+            bgColor="primary"
+            hoverBg="primary-light"
+            textColor="white"
+            className=""
+          />
         </form>
       </div>
     </div>
