feafb033f1014eefe7a0c85e482f3df483c49382
Guillermo1298 <gupena@unicauca.edu.co>
2024-10-24 22:28:24 -0500

    Componente para cambio de contraseña

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -16,6 +16,7 @@ import NavbarAdmin from "../components/navbarAdmin/NavbarAdmin";
 import Movility from "../pages/movility/Movility";
 import UpdateForm from "../pages/updateForm/UpdateForm";
 import UsersListPage from "../pages/usersListPage/UsersListPage";
+import ChangePassword from "../pages/changePassword/ChangePassword";
 
 
 function Router() {
@@ -38,6 +39,7 @@ function Router() {
             <Route path="/asideUser" element={<AsideUser/>} />
             <Route path="/asideAdmin" element={<AsideAdmin/>}/>
             <Route path="/admin/movilidad" element={<Movility />} />
+            <Route path="/changePassword" element={<ChangePassword />} />
         </Routes>
         </BrowserRouter>
     );
