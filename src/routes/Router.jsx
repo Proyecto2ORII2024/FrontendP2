6a3ddc1ff8ae33c43cdf809a861d81b06e7dd5e7
JoseENarvaezM <josenarvaezm2003@gmail.com>
2024-11-07 21:42:45 -0500

    login,cambiarcontraseña,cerrarsession y demas

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -21,30 +21,34 @@ import PasswordRecoveryEmailPage from "../pages/passwordRecoveryEmailPage/Passwo
 import { AuthProvider } from "../context/LoginContext";
 import ChangePassword from "../pages/changePassword/ChangePassword";
 
+import ProtectedRouteAdmin from "./ProtectedRouteAdmin";
+
 function Router() {
     return (
         <AuthProvider>
             <BrowserRouter>
                 <Routes>
+                    <Route element={<ProtectedRouteAdmin/>}>
+                        <Route path="/admin" element={<MainAdmin />} />
+                        <Route path="/user/list" element={<UsersListPage />} />
+                        <Route path="/statistics" element={<StatisticsPage />} />
+                        <Route path="/form" element={<FormPage />} />
+                        <Route path="/form/update/:elementId" element={<UpdateForm />} />
+                        <Route path="/show/:formId" element={<ShowMovPage />} />
+                        <Route path="/admin/agreement" element={<AgreementAdminPage />} />
+                        <Route path="/admin/agreement/create" element={<CreateAgreementPage />} />
+                        <Route path="/admin/movilidad" element={<Movility />} />
+                        <Route path="/passwordRecovery" element={<PasswordRecoveryEmailPage />} />
+                        <Route path="/changePassword" element={<ChangePassword />} />
+                    </Route>
                     <Route path="/" element={<Login />} />
-                    <Route path="/admin" element={<MainAdmin />} />
                     <Route path="/user" element={<MainUser />} />
-                    <Route path="/user/list" element={<UsersListPage />} />
-                    <Route path="/statistics" element={<StatisticsPage />} />
                     <Route path="/user/register" element={<UserRegisterPage />} />
-                    <Route path="/form" element={<FormPage />} />
-                    <Route path="/form/update/:elementId" element={<UpdateForm />} />
-                    <Route path="/show/:formId" element={<ShowMovPage />} />
-                    <Route path="/admin/agreement" element={<AgreementAdminPage />} />
-                    <Route path="/admin/agreement/create" element={<CreateAgreementPage />} />
                     <Route path="/user/agreement" element={<AgreementUserPage />} />
                     <Route path="/navbarUser" element={<NavbarUser />} />
                     <Route path="/navbarAdmin" element={<NavbarAdmin />} />
                     <Route path="/asideUser" element={<AsideUser />} />
                     <Route path="/asideAdmin" element={<AsideAdmin />} />
-                    <Route path="/admin/movilidad" element={<Movility />} />
-                    <Route path="/passwordRecovery" element={<PasswordRecoveryEmailPage />} />
-                    <Route path="/changePassword" element={<ChangePassword />} />
                 </Routes>
             </BrowserRouter>
         </AuthProvider >
