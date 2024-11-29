5ddee29bab8f41706550f40c3ac0499e6f410720
IyorchsI <joanrosero@unicacua.edu.co>
2024-11-28 22:53:27 -0500

    small change in form page

--- src/utils/facultyOptions.js
+++ src/utils/facultyOptions.js
@@ -4,14 +4,14 @@
  */
 
 const facultyOptions = [
-    { value: "FIET", text: "Ingeniería Electrónica y Telecomunicaciones" },
-    { value: "FIC", text: "Ingeniería Civil" },
-    { value: "FCS", text: "Ciencias de la Salud" },
-    { value: "FDCPS", text: "Derecho y Ciencias Políticas y Sociales" },
+    { value: "FIET", text: "Facultad de Ingeniería Electrónica y Telecomunicaciones" },
+    { value: "FIC", text: "Facultad de Ingeniería Civil" },
+    { value: "FCS", text: "Facultad de Ciencias de la Salud" },
+    { value: "FDCPS", text: "Facultad de Derecho y Ciencias Políticas y Sociales" },
     { value: "FACNED", text: "Facultad de Ciencias Naturales, Exactas y de la Educación" },
-    { value: "FCH", text: "Ciencias Humanas" },
-    { value: "FA", text: "Artes" },
-    { value: "FCA", text: "Ciencias Agropecuarias" },
-    { value: "FCCEA", text: "Ciencias Contables, Económicas y Administrativas" },
+    { value: "FCH", text: "Facultad de Ciencias Humanas" },
+    { value: "FA", text: "Facultad de Artes" },
+    { value: "FCA", text: "Facultad de Ciencias Agropecuarias" },
+    { value: "FCCEA", text: "Facultad de Ciencias Contables, Económicas y Administrativas" },
 ];
 export default facultyOptions;
